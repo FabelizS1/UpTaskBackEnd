89368108c9ec2250a8f174abf0b8999019fe74ac
Fabeliz Alvarez <fabelizfi1@gmail.com>
2024-09-11 17:19:20 -0400

    update

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -5,10 +5,10 @@ type UserPayload = {
     id: Types.ObjectId  // Como se le pasa un objeto se coloca como Types.ObjectId
 }
 
-export const generateJWT = (id: UserPayload) => {
+export const generateJWT = (payload: UserPayload) => {
 
     // El json web token se esta generando con el id que sale de UserPayload
-    const token = jwt.sign(id, process.env.JWT_SECRET, { // Con sign se genera el json web token, el parametro de JWT_SECRET es una variable de entorno esta se va a utilizar para generar y validar el token
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { // Con sign se genera el json web token, el parametro de JWT_SECRET es una variable de entorno esta se va a utilizar para generar y validar el token
         expiresIn: '180d' // Tiempo de validez del json web token
     })
     return token
